5a8f55a1b650a991dbdfde7544a5c8f45ee2c341
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2024-04-29 12:18:51 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../../components/Header'; // Importe corretamente o componente Header
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> {/* Use o componente Header aqui */}
+    </div>
+  );
+}
+
+export default App;
